adb06f6f23b382b7cf557a70a464fe1cf8bb3c4e
HeunG22 <101494137+HeunG22@users.noreply.github.com>
2024-07-05 19:42:53 +0900

    WallMove 구현
    idle/walk/aerial 에서 좌클릭 -> PullWall-> WallIdle/WallMove -> PushWall(Player 원래대로)
    - PullWall : 벽에 수직이 되도록 player 방향 전환
    - WallMove : local vector로 움직임

--- FSMPractice/Assets/Scripts/Interaction/InteractionManager.cs
+++ FSMPractice/Assets/Scripts/Interaction/InteractionManager.cs
@@ -9,7 +9,7 @@ public class InteractionManager : MonoBehaviour
     [SerializeField] private InputReader _inputReader = default;
 
     // Events for the different interaction types
-    // Type º°·Î OnTrigger·Î Ã³¸®ÇØÁà¾ßÇØ. ConditionÀ» ¼³Á¤ÇØ ÇØ´ç state·Î ³Ñ¾î°¥ ¼ö ÀÖ°Ô
+    // Type ï¿½ï¿½ï¿½ï¿½ OnTriggerï¿½ï¿½ Ã³ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½. Conditionï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ø´ï¿½ stateï¿½ï¿½ ï¿½Ñ¾î°¥ ï¿½ï¿½ ï¿½Ö°ï¿½
 
     [ReadOnly] public InteractionType currentInteractionType;
     [ReadOnly] public GameObject currentInteractiveObject;
@@ -59,6 +59,7 @@ public class InteractionManager : MonoBehaviour
 
         if (obj.CompareTag("Light")) newPotentialInteraction.type = InteractionType.Light;
         else if (obj.CompareTag("Heavy")) newPotentialInteraction.type = InteractionType.Heavy;
+        else if (obj.CompareTag("Wall")) newPotentialInteraction.type = InteractionType.Wall;
 
 
         if (newPotentialInteraction.type != InteractionType.None)
---
+++ FSMPractice/Assets/Scripts/Player/StateMachine/Conditions/isPushingWallConditionSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Test.StateMachine;
+using Test.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "isPushingWallCondition", menuName = "State Machines/Conditions/is Pushing Wall Condition")]
+public class isPushingWallConditionSO : StateConditionSO<isPushingWallCondition> { }
+
+public class isPushingWallCondition : Condition
+{
+	protected new isPushingWallConditionSO OriginSO => (isPushingWallConditionSO)base.OriginSO;
+	private InteractionManager _interactionManager;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_interactionManager = stateMachine.GetComponent<InteractionManager>();
+	}
+
+	protected override bool Statement() => _interactionManager.pushInput;
+}
