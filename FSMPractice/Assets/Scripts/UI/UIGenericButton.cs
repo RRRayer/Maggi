5f1511482999360b7197d05c7ef1eaafa026c73c
jung <jungse8609@naver.com>
2024-07-21 02:05:17 +0900

    [FEATURE] Hierarchy에 PauseUI 구성

--- FSMPractice/Assets/Scripts/UI/UIGenericButton.cs
+++ FSMPractice/Assets/Scripts/UI/UIGenericButton.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UIGenericButton : MonoBehaviour
 {
     [SerializeField] private MultiInputButton _button = default;
+
+    public UnityAction Clicked = default;
+
 
 
 }
