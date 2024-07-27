3246ec30ec2d0984e09fdaabaf7110f54a49b640
Kukkyeung <jungse8609@naver.com>
2024-07-28 02:30:50 +0900

    [FEATURE] Add Timeline Test

---
+++ FSMPractice/Assets/Timeline/script.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class script : MonoBehaviour
+{
+    public void OnReceivedSignal()
+    {
+        Debug.Log("¿Ã ∏ˆ µÓ¿Â!!");
+    }
+}
