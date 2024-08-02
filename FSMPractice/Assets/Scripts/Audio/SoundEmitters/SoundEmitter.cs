5571efb843f56f605f8482461216815a1af34704
Kukkyeung <jungse8609@naver.com>
2024-08-02 18:16:17 +0900

    [FEATURE] AudioManager 추가

--- FSMPractice/Assets/Scripts/Audio/SoundEmitters/SoundEmitter.cs
+++ FSMPractice/Assets/Scripts/Audio/SoundEmitters/SoundEmitter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using DG.Tweening;
 
 public class SoundEmitter : MonoBehaviour
 {
@@ -29,6 +30,64 @@ public class SoundEmitter : MonoBehaviour
         {
             StartCoroutine(FinishedPlaying(clip.length));
         }
+    }
+
+    public void FadeMusicIn(AudioClip musicClip, AudioConfigurationSO settings, float duration, float startTime = 0f)
+    {
+        PlayAudioClip(musicClip, settings, true);
+        _audioSource.volume = 0f;
+
+        //Start the clip at the same time the previous one left, if length allows
+        //TODO: find a better way to sync fading songs
+        if (startTime <= _audioSource.clip.length)
+            _audioSource.time = startTime;
+
+        _audioSource.DOFade(settings.Volume, duration);
+    }
+
+    public float FadeMusicOut(float duration)
+    {
+        _audioSource.DOFade(0f, duration).onComplete += OnFadeOutComplete;
+
+        return _audioSource.time;
+    }
+
+    private void OnFadeOutComplete()
+    {
+        NotifyBeingDone();
+    }
+
+    /// <summary>
+	/// Used to check which music track is being played.
+	/// </summary>
+	public AudioClip GetClip()
+    {
+        return _audioSource.clip;
+    }
+
+    public void Stop()
+    {
+        _audioSource.Stop();
+    }
+
+    public void Finish()
+    {
+        if (_audioSource.loop)
+        {
+            _audioSource.loop = false;
+            float timeRemaining = _audioSource.clip.length - _audioSource.time;
+            StartCoroutine(FinishedPlaying(timeRemaining));
+        }
+    }
+
+    public bool IsPlaying()
+    {
+        return _audioSource.isPlaying;
+    }
+
+    public bool IsLooping()
+    {
+        return _audioSource.loop;
     }
 
     private IEnumerator FinishedPlaying(float clipLength)
