5c04af485e2094093284d5321e315cb680be43b8
HeunG22 <101494137+HeunG22@users.noreply.github.com>
2024-08-14 00:43:45 +0900

    Resolved merge conflict in Point.mat

---
+++ FSMPractice/Assets/Scripts/Characters/StateMachine/Actions/SieveWheel/RideSieveWheelActionSO.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Pudding.StateMachine;
+using Pudding.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "RideSieveWheelAction", menuName = "State Machines/Actions/Ride Sieve Wheel Action")]
+public class RideSieveWheelActionSO : StateActionSO
+{
+	protected override StateAction CreateAction() => new RideSieveWheelAction();
+}
+
+public class RideSieveWheelAction : StateAction
+{
+	protected new RideSieveWheelActionSO _originSO => (RideSieveWheelActionSO)base.OriginSO;
+
+    private Transform _transform;
+	private InteractionManager _interactionManager;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+        _transform = stateMachine.GetComponent<Transform>();
+        _interactionManager = stateMachine.GetComponent<InteractionManager>();
+	}
+
+    public override void OnStateEnter()
+    {
+        _transform.rotation = _interactionManager.currentInteractiveObject.transform.rotation;
+        _transform.position = _interactionManager.currentInteractiveObject.transform.position
+							+ _interactionManager.currentInteractiveObject.transform.up * -0.3f;
+    }
+
+    public override void OnUpdate() { }
+}
---
+++ FSMPractice/Assets/Scripts/Characters/StateMachine/Conditions/isPullingSieveWheelConditionSO.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Pudding.StateMachine;
+using Pudding.StateMachine.ScriptableObjects;
+
+[CreateAssetMenu(fileName = "isPullingSieveWheelCondition", menuName = "State Machines/Conditions/is Pulling Sieve Wheel Condition")]
+public class isPullingSieveWheelConditionSO : StateConditionSO<isPullingSieveWheelCondition> { }
+
+public class isPullingSieveWheelCondition : Condition
+{
+	private InteractionManager _interactionManager;
+
+	public override void Awake(StateMachine stateMachine)
+	{
+		_interactionManager = stateMachine.GetComponent<InteractionManager>();
+	}
+	
+	protected override bool Statement()
+	{
+		if (_interactionManager.currentInteractionType == InteractionType.SieveWheel)
+			return true;
+		return false;
+	}
+}
