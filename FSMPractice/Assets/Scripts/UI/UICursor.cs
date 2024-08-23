4b79c8225de6d495f31b6f2efedc046ee19a27eb
Kukkyeung <jungse8609@naver.com>
2024-08-23 20:10:59 +0900

    [FEATURE] 벽 로직 버그 수정

--- FSMPractice/Assets/Scripts/UI/UICursor.cs
+++ FSMPractice/Assets/Scripts/UI/UICursor.cs
@@ -1,13 +1,21 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UICursor : MonoBehaviour
 {
     [SerializeField] private RectTransform cursorImage;
+    [SerializeField] private CanvasGroup cursorCanvasGroup;
 
     private void Start()
     {
         // 기본 시스템 커서 숨기기
         Cursor.visible = false;
+
+        // UI 커서가 클릭 이벤트를 방해하지 않도록 설정
+        if (cursorCanvasGroup != null)
+        {
+            cursorCanvasGroup.blocksRaycasts = false;
+        }
     }
 
     private void Update()
